96e3ef83a4efc7f44fd0d1eef34436e9e108957c
sehr <sehr.sehr1112@gmail.com>
2023-12-22 15:10:59 +0500

    Added mysql

--- DatabaseModel.java
+++ DatabaseModel.java
@@ -1,4 +1,15 @@
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class DatabaseModel {
-	{}
+	public DatabaseModel{
+		 try {
+	            // Establishing the database connection
+	            Class.forName("com.mysql.cj.jdbc.Driver");
+	            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/wasterecycledb?serverTimezone=UTC", "root", "root");
+	            System.out.println("Connected to the database!");
+	        } catch (ClassNotFoundException | SQLException e) {
+	            e.printStackTrace();
+	        }
+	}
 }
