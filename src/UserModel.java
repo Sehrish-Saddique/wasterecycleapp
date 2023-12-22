1ccdd8b1b70a527e9359f2377b921e973c70448f
sehr <sehr.sehr1112@gmail.com>
2023-12-22 14:08:25 +0500

    added usercode

--- src/UserModel.java
+++ src/UserModel.java
@@ -1,8 +1,11 @@
-
-public class UserModel {
+import java.util.ArrayList;
+ public class UserModel {
 
 	public UserModel() {
 		// TODO Auto-generated constructor stub
+		System.out.println("UserModel");
 	}
 
+	public static void main(String[] args){
+		System.out.println("UserModel");}
 }
